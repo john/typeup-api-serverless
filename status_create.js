2e528adaad70e5aea6ebd97672630cf4ef2dfb98
john <john@fryolator.com>
2017-08-16 09:07:59 -0700

    handle empty content on status create

--- status_create.js
+++ status_create.js
@@ -5,37 +5,56 @@ import { success, failure } from './libs/response-lib';
 export async function main(event, context, callback) {
   const data = JSON.parse(event.body);
   const statusId = uuid.v1();
+  
+  var statusItem ={
+    statusId: statusId,
+    cognitoIdentityId: event.requestContext.identity.cognitoIdentityId,
+    userName: data.userName,
+    title: data.title,
+    content: null,
+    userState: data.userState,
+    attachment: data.attachment,
+    createdAt: new Date().toISOString(),
+  }
+  
+  // var userUpdateExpression = 'set last_status_id=:sid, last_status_title = :t, last_status_createdAt = :ca, last_status_attachment = :lsa'
+  // var userAttributeValues = {
+  //   ":sid": statusId,
+  //   ":t": data.title,
+  //   ":ca": new Date().toISOString(),
+  //   ":lsa": data.attachment,
+  // }
+  var userUpdateExpression = 'set last_status_id=:sid, last_status_title = :t, last_status_content = :c, last_status_createdAt = :ca, last_status_attachment = :lsa'
+  var userAttributeValues = {
+    ":sid": statusId,
+    ":t": data.title,
+    ":c": null,
+    ":ca": new Date().toISOString(),
+    ":lsa": data.attachment,
+  }
+  
+  if( data.content ) {
+    statusItem.content = data.content
+    userAttributeValues[":c"] = data.content
+  }
+  // if( data.content ) {
+  //   statusItem.content = data.content
+  //   userUpdateExpression += ', last_status_content = :c'
+  //   userAttributeValues[":c"] = data.content
+  // }
+    
   const status_params = {
     TableName: 'statuses',
-    Item: {
-      statusId: statusId,
-      cognitoIdentityId: event.requestContext.identity.cognitoIdentityId,
-      userName: data.userName,
-      title: data.title,
-      content: data.content,
-      userState: data.userState,
-      attachment: data.attachment,
-      createdAt: new Date().toISOString(),
-    },
+    Item: statusItem,
   };
-
-// get User, so we can get the name. Would be good to figure out how to get that from the session in the client, and pass it through.
-  // const user_fetch_params = {
-  //
-  // }
   
   const user_update_params = {
     TableName: 'users',
     Key: {
       userName: data.userName,
     },
-    UpdateExpression: 'set last_status_id=:sid, last_status_title = :t, last_status_content = :c, last_status_createdAt = :ca',
-    ExpressionAttributeValues:{
-      ":sid": statusId,
-      ":t": data.title,
-      ":c": data.content,
-      ":ca": new Date().toISOString(),
-    },
+    UpdateExpression: userUpdateExpression,
+    ExpressionAttributeValues: userAttributeValues,
     ReturnValues:"UPDATED_NEW"
   }
 
